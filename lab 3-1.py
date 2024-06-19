831c3dcf7bb978cced54c8fbf09fd8512cf55e44
gfvdfg <phumi.kehanadee@gmail.com>
2024-06-19 15:38:54 +0700

    Add files via upload

---
+++ lab 3-1.py
@@ -0,0 +1,5 @@
+score = int(input("score ="))
+while score != -1:
+    print (score)
+    score = int(input("score ="))
+
---
+++ lab 3-4.py
@@ -0,0 +1,5 @@
+x = 0
+for i in range(5):
+    z = int(input("in put: "))
+    x += z
+    print("=", x)
